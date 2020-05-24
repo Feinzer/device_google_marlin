228e6b92ed4b5f92d1d497da2e0e01b3f5aee146
Feinzer <danhernan23i@gmail.com>
2020-05-24 13:28:51 -0400

    Nuke lineage overlays

--- device-lineage.mk
+++ device-lineage.mk
@@ -23,9 +23,6 @@ PRODUCT_PACKAGES += \
     RcsService \
     PresencePolling
 
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += device/google/marlin/overlay-lineage
-
 # Sensors
 PRODUCT_PACKAGES += \
     libsensorndkbridge
