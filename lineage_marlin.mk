6a0b99110dd8f180d8bd8b858f0bfea111ca0d64
Feinzer <danhernan23i@gmail.com>
2020-05-24 13:31:35 -0400

    Remove unnecessary aosp_marlin.mk

--- lineage_marlin.mk
+++ lineage_marlin.mk
@@ -5,13 +5,15 @@ TARGET_SCREEN_WIDTH := 1440
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/google/marlin/aosp_marlin.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 -include device/google/marlin/marlin/device-lineage.mk
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_marlin
+PRODUCT_DEVICE := marlin
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel XL
 TARGET_MANUFACTURER := HTC
@@ -23,4 +25,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT := google/marlin/marlin:8.1.0/OPM1.171019.021/4565141:user/release-keys
 
+$(call inherit-product, device/google/marlin/device-marlin.mk)
 $(call inherit-product-if-exists, vendor/google/marlin/marlin-vendor.mk)
